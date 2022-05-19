8b0fa2ce83d1a0dae20ae5a824a75136223b1339
Dor Danai <danaidor@gmail.com>
2022-05-19 20:44:50 +0300

    simplifying tests

--- script.ps1
+++ script.ps1
@@ -2,24 +2,24 @@ param(
     [Parameter()]
     [String]$RequestedCocktail
 )
+$Env:MyVariable
 
-write-host 'env var ' $Env:MyVariable
-$res = Invoke-RestMethod "www.thecocktaildb.com/api/json/v1/1/search.php?s=$($Env:MyVariable)"  
-$drinkName =$res.drinks[0].strDrink
-$glassType = $res.drinks[0].strGlass
-$ingredients = New-Object Collections.Generic.List[string]
+# $res = Invoke-RestMethod "www.thecocktaildb.com/api/json/v1/1/search.php?s=$($Env:MyVariable)"  
+# $drinkName =$res.drinks[0].strDrink
+# $glassType = $res.drinks[0].strGlass
+# $ingredients = New-Object Collections.Generic.List[string]
 
-foreach ($param in $res.drinks[0] | Get-Member){
+# foreach ($param in $res.drinks[0] | Get-Member){
     
-   if ($param -Match "strIngredient" -and  $param -inotmatch "null"){
-        $pos = $param.Definition.IndexOf("=")              
-        $rightPart = $param.Definition.Substring($pos+1)
-        $ingredients.add("`n`t$($rightPart)")
-    }
-}
+#    if ($param -Match "strIngredient" -and  $param -inotmatch "null"){
+#         $pos = $param.Definition.IndexOf("=")              
+#         $rightPart = $param.Definition.Substring($pos+1)
+#         $ingredients.add("`n`t$($rightPart)")
+#     }
+# }
 
  
-write-host "Drink Name: " $drinkName 
-write-host "Glass Type: " $glassType
-write-host "Ingredients: " $ingredients | Format-List
+# write-host "Drink Name: " $drinkName 
+# write-host "Glass Type: " $glassType
+# write-host "Ingredients: " $ingredients | Format-List
 
