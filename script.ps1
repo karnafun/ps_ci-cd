d3571738e6bed1a0c84d278977fe9d3b7e932704
Dor Danai <danaidor@gmail.com>
2022-05-19 19:50:56 +0300

    adding results for mojito cocktail

---
+++ script.ps1
@@ -0,0 +1,22 @@
+
+$RequestedCocktail = "mojito"
+
+$res = Invoke-RestMethod "www.thecocktaildb.com/api/json/v1/1/search.php?s=$($RequestedCocktail)"  
+$drinkName =$res.drinks[0].strDrink
+$glassType = $res.drinks[0].strGlass
+$ingredients = New-Object Collections.Generic.List[string]
+
+foreach ($param in $res.drinks[0] | Get-Member){
+    
+   if ($param -Match "strIngredient" -and  $param -inotmatch "null"){
+        $pos = $param.Definition.IndexOf("=")              
+        $rightPart = $param.Definition.Substring($pos+1)
+        $ingredients.add("`n`t$($rightPart)")
+    }
+}
+
+ 
+write-host "Drink Name: " $drinkName 
+write-host "Glass Type: " $glassType
+write-host "Ingredients: " $ingredients | Format-List
+
