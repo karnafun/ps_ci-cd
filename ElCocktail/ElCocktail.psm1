0c1d2a5eca7c56d75681d8ce45ce0319f2267d2c
Dor Danai <danaidor@gmail.com>
2022-05-20 13:20:18 +0300

    adding ingredients list to cocktails and publish workflow for main branch

--- ElCocktail/ElCocktail.psm1
+++ ElCocktail/ElCocktail.psm1
@@ -1,21 +1,63 @@
+Class CocktailIngredient{
+    [string]$name
+    [string]$amount
+    CocktailIngredient([string]$ingName,[string]$ingAmount){
+        $this.name = $ingName
+        $this.amount = $ingAmount
+    }
+}
+
 Class Cocktail{
     [string]$name
     [string]$glassType
-    [string[]]$ingredients
+    [CocktailIngredient[]]$ingredients
 
     Cocktail([string]$cName){
         $data = Invoke-RestMethod "www.thecocktaildb.com/api/json/v1/1/search.php?s=$($cName)"  
         $this.name =$data.drinks[0].strDrink
         $this.glassType = $data.drinks[0].strGlass
-        foreach ($param in $data.drinks[0] | Get-Member){
-            
-        if ($param -Match "strIngredient" -and  $param -inotmatch "null"){
+        $ingNames =new-Object Collections.Generic.List[string]
+        $ingValues=new-Object Collections.Generic.List[string]
+        
+        $ing = [CocktailIngredient]::new('water','a lot')
+        foreach ($param in $data.drinks[0] | Get-Member){            
+            if ($param -Match "strIngredient" -and  $param -inotmatch "null"){
+                    $pos = $param.Definition.IndexOf("=")              
+                    $rightPart = $param.Definition.Substring($pos+1)
+                    $ingNames.add("$($rightPart)")                     
+            }elseif ($param -Match "measure" -and  $param -inotmatch "null"){
                 $pos = $param.Definition.IndexOf("=")              
-                $rightPart = $param.Definition.Substring($pos+1)
-                $this.ingredients.add("`n`t$($rightPart)")
+                    $rightPart = $param.Definition.Substring($pos+1)
+                    $ingValues.add("$($rightPart)")   
             }
         }
+    
+        for (($i = 0); $i -lt $ingNames.Count; $i++)
+            {
+                $ingName = $ingNames[$i]
+                $ingValue = ''
+                if ($i -lt $ingValues.Count){ $ingValue = $ingValues[$i]}
+                $this.ingredients+= [CocktailIngredient]::new($ingName,$ingValue)
+        }        
+
+        
     }
+    [void] GetEvenIntegers(){
+        # this following line doesn't go to the pipeline
+        $this.Integers.Where({ ($_ % 2) -eq 0})
+    }
+
+    [void]PrintIngredients(){
+        foreach ($ing in $this.ingredients) {
+            if([string]::IsNullOrEmpty($ing.amount)){
+                write-host "`n $($ing.name)"
+            }else {
+                write-host "`n $($ing.name) - $($ing.amount) "
+            }
+            
+        }
+    }
+ 
 }
 function GetCocktail([string]$name){
     return [Cocktail]::new($name)
